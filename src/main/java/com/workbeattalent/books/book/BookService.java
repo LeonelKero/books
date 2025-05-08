488967634cd9f0e7db417b2bd7e515fb40e2db8e
leonel ka <waboleonel@gmail.com>
2025-05-08 18:53:25 +0100

    Implement book service layer

---
+++ src/main/java/com/workbeattalent/books/book/BookService.java
@@ -0,0 +1,56 @@
+package com.workbeattalent.books.book;
+
+import com.workbeattalent.books.author.AuthorService;
+import com.workbeattalent.books.dto.BookRequest;
+import com.workbeattalent.books.dto.BookResponse;
+import com.workbeattalent.books.exceptions.EntityElementNotFoundException;
+import com.workbeattalent.books.util.EntityDtoMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+    private final BookRepository repository;
+    private final AuthorService authorService;
+    private final EntityDtoMapper mapper;
+
+    public BookResponse store(final BookRequest bookRequest) {
+        final var authors = this.authorService.findAll(bookRequest.authorIds());
+        if (authors.size() != bookRequest.authorIds().size()) {
+            throw new EntityElementNotFoundException("One or more author not found");
+        }
+        final var book = this.mapper.toBook(bookRequest);
+        book.setAuthors(authors);
+        final var savedBook = this.repository.save(book);
+        return this.mapper.fromBook(savedBook);
+    }
+
+
+    public List<BookResponse> findAll() {
+        return this.repository.findAll().stream()
+                .map(this.mapper::fromBook)
+                .toList();
+    }
+
+    public BookResponse findById(final Long bookId) {
+        return this.repository.findById(bookId)
+                .map(this.mapper::fromBook)
+                .orElseThrow(() -> new EntityElementNotFoundException("No Book found with id: " + bookId));
+    }
+
+    public Long delete(final Long bookId) {
+        return this.repository.findById(bookId)
+                .map(book -> {
+                    this.repository.delete(book);
+                    return book.getId();
+                })
+                .orElseThrow(() -> new EntityElementNotFoundException("Unable to delete book with id: " + bookId));
+    }
+
+    public BookResponse update(final Long bookId, final BookRequest newUpdates) {
+        return null;
+    }
+}
