0eefbeef3588db644a86f2756f9deabdef1d942c
leonel ka <waboleonel@gmail.com>
2025-05-09 07:08:04 +0100

    Update id type

--- src/main/java/com/workbeattalent/books/dto/AuthorRequest.java
+++ src/main/java/com/workbeattalent/books/dto/AuthorRequest.java
@@ -4,8 +4,10 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+import java.util.UUID;
+
 public record AuthorRequest(
-        Long id,
+        UUID id,
 
         @NotBlank(message = "Author firstname is required")
         @Size(min = 2, message = "Author firstname should be on at least 02 characters")
