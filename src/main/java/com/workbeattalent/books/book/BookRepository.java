5f2c52e4e3977d63e8f7d3a992e183898ea3352e
leonel ka <waboleonel@gmail.com>
2025-05-09 07:01:40 +0100

    Add custom methods for getting books

--- src/main/java/com/workbeattalent/books/book/BookRepository.java
+++ src/main/java/com/workbeattalent/books/book/BookRepository.java
@@ -1,8 +1,14 @@
 package com.workbeattalent.books.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findByTitleLikeIgnoreCaseOrderByAuthors_FirstnameAsc(@NonNull String title);
+
+    List<Book> findByAuthors_FirstnameIgnoreCaseOrAuthors_LastnameIgnoreCase(@NonNull String firstname, String lastname);
 }
