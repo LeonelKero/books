190a7f193739e40cea5af3967aa5297af5eb1886
leonel ka <waboleonel@gmail.com>
2025-05-14 21:47:04 +0100

    Complete testing the API Controller layer

--- src/test/java/com/workbeattalent/books/rest/ApiControllerTest.java
+++ src/test/java/com/workbeattalent/books/rest/ApiControllerTest.java
@@ -3,8 +3,10 @@ package com.workbeattalent.books.rest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.workbeattalent.books.author.Author;
 import com.workbeattalent.books.book.BookService;
+import com.workbeattalent.books.dto.AuthorRequest;
 import com.workbeattalent.books.dto.BookRequest;
 import com.workbeattalent.books.dto.BookResponse;
+import com.workbeattalent.books.exceptions.EntityElementNotFoundException;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -208,7 +210,44 @@ class ApiControllerTest {
 
 
     @Test
-    void searchAuthorBooks() {
+    void givenValidAuthorRequest_whenRetrievingBooksByAuthor_thenReturnListOfBooksWithStatusOK() throws Exception {
+        // Given
+        final var authorId = UUID.randomUUID();
+        final var greg = Author.builder()
+                .id(authorId)
+                .firstname("Greg L.")
+                .lastname("Turnsquist")
+                .email("turnquist@hotmail.com")
+                .build();
+        final var book = new BookResponse(
+                1L,
+                "Learning Spring Boot 3.0",
+                248,
+                "Simplify the development of production grade application with java and spring boot",
+                Set.of(greg));
+        final var authorRequest = new AuthorRequest(authorId, "Greg L.", "Turnsquist", "turnquist@hotmail.com");
+        when(this.bookService.findByAuthorName(authorRequest)).thenReturn(List.of(book));
+        // When // Then
+        mvc.perform(MockMvcRequestBuilders.post(API_URI + "/authors")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(this.mapper.writeValueAsString(authorRequest)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.size()", Matchers.is(1)));
+    }
+
+    @Test
+    void givenCompleteInvalidAuthorRequest_whenRetrievingBooksByAuthor_thenReturnBAD_REQUEST() throws Exception {
+        // Given
+        final var authorRequest = new AuthorRequest(UUID.randomUUID(), null, null, "turnquisthotmail.com");
+        // When // Then
+        mvc.perform(MockMvcRequestBuilders.post(API_URI + "/authors")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(this.mapper.writeValueAsString(authorRequest)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.errors['firstname']", Matchers.is("Author firstname is required")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.errors['email']", Matchers.is("Invalid email")));
+        verify(this.bookService, never()).findByAuthorName(any());
+
     }
 
     @Test
@@ -225,15 +264,111 @@ class ApiControllerTest {
     }
 
     @Test
-    void getBook() {
+    void givenIdOfExistingBook_whenGettingBookByItsId_thenReturnBookWithStatusOK() throws Exception {
+        // Given
+        final var greg = Author.builder()
+                .id(UUID.randomUUID())
+                .firstname("Greg L.")
+                .lastname("Turnsquist")
+                .email("turnquist@hotmail.com")
+                .build();
+        final var id = 1L;
+        final var response = new BookResponse(
+                id,
+                "Learning Spring Boot 3.0",
+                248,
+                "Simplify the development of production grade application with java and spring boot",
+                Set.of(greg));
+        when(this.bookService.findById(id)).thenReturn(response);
+        // When // Then
+        mvc.perform(MockMvcRequestBuilders.get(API_URI + "/{id}", id))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.title", Matchers.is(response.title())));
+    }
+
+
+    @Test
+    void givenIdOfNotExistingBook_whenGettingBookByItsId_thenReturnNOT_FOUND() throws Exception {
+        // Given
+        final var id = -1L;
+        when(this.bookService.findById(id)).thenThrow(new EntityElementNotFoundException("No Book found with id: " + id));
+        // When // Then
+        mvc.perform(MockMvcRequestBuilders.get(API_URI + "/{id}", id))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message", Matchers.is("No Book found with id: " + id)));
     }
 
     @Test
-    void delete() {
+    void givenIdOfExistingBook_whenDeletingBook_thenReturnACCEPTED() throws Exception {
+        // Given
+        final var bookId = 1L;
+        when(this.bookService.delete(bookId)).thenReturn(bookId);
+        // When // Then
+        mvc.perform(MockMvcRequestBuilders.delete(API_URI + "/{id}", bookId))
+                .andExpect(MockMvcResultMatchers.status().isAccepted())
+                .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.notNullValue()));
     }
 
     @Test
-    void update() {
+    void givenIdOfNotExistingBook_whenDeletingBook_thenReturnNOT_FOUND() throws Exception {
+        // Given
+        final var bookId = -1L;
+        when(this.bookService.delete(bookId)).thenThrow(new EntityElementNotFoundException("Unable to delete book with id: " + bookId));
+        // When // Then
+        mvc.perform(MockMvcRequestBuilders.delete(API_URI + "/{id}", bookId))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message", Matchers.is("Unable to delete book with id: " + bookId)));
     }
 
+    @Test
+    void givenValidBookRequest_whenTyingToUpdateBookById_thenReturnUpdatedBookWithStatusACCEPTED() throws Exception {
+        // Given
+        final var greg = Author.builder()
+                .id(UUID.randomUUID())
+                .firstname("Greg L.")
+                .lastname("Turnsquist")
+                .email("turnquist@hotmail.com")
+                .build();
+        final var id = 1L;
+        final var response = new BookResponse(
+                id,
+                "Spring Boot 7.0",
+                200,
+                "Production grade application with latest spring boot",
+                Set.of(greg));
+        final var request = new BookRequest(
+                id,
+                "Spring Boot 7.0",
+                200,
+                "Production grade application with latest spring boot",
+                Set.of(greg.getId()));
+        when(this.bookService.update(id, request)).thenReturn(response);
+        // When // Then
+        mvc.perform(MockMvcRequestBuilders.put(API_URI + "/{id}", id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(this.mapper.writeValueAsString(request)))
+                .andExpect(MockMvcResultMatchers.status().isAccepted())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.authors[0].firstname", Matchers.is(greg.getFirstname())));
+    }
+
+    @Test
+    void givenNotValidBookRequest_whenTyingToUpdateBookById_thenReturnBAD_REQUEST() throws Exception {
+        // Given
+        final var fakeId = -1L;
+        final var request = new BookRequest(
+                null,
+                null,
+                null,
+                "Production grade application with latest spring boot",
+                null);
+        // When // Then
+        mvc.perform(MockMvcRequestBuilders.put(API_URI + "/{id}", fakeId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(this.mapper.writeValueAsString(request)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.errors['title']", Matchers.is("Book title is required")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.errors['pages']", Matchers.is("Book page numbers is required")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.errors['authorIds']", Matchers.is("Please provide an authors id list")));
+        verify(this.bookService, never()).update(anyLong(), any(BookRequest.class));
+    }
 }
