847ef2d34114ad6b0f41f30a48918ba82eff4cea
leonel ka <waboleonel@gmail.com>
2025-05-14 13:25:05 +0100

    Finalize author controller test

--- src/test/java/com/workbeattalent/books/rest/AuthorControllerTest.java
+++ src/test/java/com/workbeattalent/books/rest/AuthorControllerTest.java
@@ -1,22 +1,25 @@
 package com.workbeattalent.books.rest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.workbeattalent.books.author.AuthorService;
+import com.workbeattalent.books.dto.AuthorRequest;
 import com.workbeattalent.books.dto.AuthorResponse;
 import com.workbeattalent.books.exceptions.EntityElementNotFoundException;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.Map;
-import java.util.Set;
 import java.util.UUID;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @WebMvcTest(controllers = {AuthorController.class})
 class AuthorControllerTest {
@@ -25,6 +28,9 @@ class AuthorControllerTest {
 
     @Autowired
     private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper mapper;
 
     @MockitoBean
     private AuthorService authorService;
@@ -63,6 +69,76 @@ class AuthorControllerTest {
     }
 
     @Test
-    void author() {
+    void givenValidAuthor_whenRequestForCreation_thenCreateNewAuthor() throws Exception {
+        // Given
+        final var request = new AuthorRequest(null, "John", "Doe", "john.doe@mail.org");
+        final var response = new AuthorResponse(UUID.randomUUID(), "John", "Doe", "john.doe@mail.org");
+        when(this.authorService.create(request)).thenReturn(response);
+        // When // Thens
+        mvc.perform(MockMvcRequestBuilders.post(API_PATH)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(this.mapper.writeValueAsString(request)))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.notNullValue()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.firstname", Matchers.is(request.firstname())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.lastname", Matchers.is(request.lastname())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email", Matchers.is(request.email())));
+    }
+
+    @Test
+    void givenInvalidAuthorFirstname_whenRequestForCreation_thenReturnBadRequest() throws Exception {
+        // Given
+        final var request = new AuthorRequest(null, null, "Doe", "john.doe@mail.org");
+        // When // Then
+        mvc.perform(MockMvcRequestBuilders.post(API_PATH)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(this.mapper.writeValueAsString(request)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.errors['firstname']", Matchers.is("Author firstname is required")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.status", Matchers.is(HttpStatus.BAD_REQUEST.name())));
+        verify(this.authorService, never()).create(any(AuthorRequest.class));
+    }
+
+    @Test
+    void givenInvalidAuthorFirstnameLength_whenRequestForCreation_thenReturnBadRequest() throws Exception {
+        // Given
+        final var request = new AuthorRequest(null, "k", "Doe", "john.doe@mail.org");
+        // When // Then
+        mvc.perform(MockMvcRequestBuilders.post(API_PATH)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(this.mapper.writeValueAsString(request)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.errors['firstname']", Matchers.is("Author firstname should be on at least 02 characters")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.status", Matchers.is(HttpStatus.BAD_REQUEST.name())));
+        verify(this.authorService, never()).create(any(AuthorRequest.class));
+    }
+
+    @Test
+    void givenInvalidAuthorEmail_whenRequestForCreation_thenReturnBadRequest() throws Exception {
+        // Given
+        final var request = new AuthorRequest(null, "kye", "Doe", "john.doemail.org");
+        // When // Then
+        mvc.perform(MockMvcRequestBuilders.post(API_PATH)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(this.mapper.writeValueAsString(request)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.errors['email']", Matchers.is("Invalid email")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.status", Matchers.is(HttpStatus.BAD_REQUEST.name())));
+        verify(this.authorService, never()).create(any(AuthorRequest.class));
+    }
+
+    @Test
+    void givenInvalidAuthorEmailAndFirstname_whenRequestForCreation_thenReturnBadRequest() throws Exception {
+        // Given
+        final var request = new AuthorRequest(null, null, "Doe", "john.doemail.org");
+        // When // Then
+        mvc.perform(MockMvcRequestBuilders.post(API_PATH)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(this.mapper.writeValueAsString(request)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.errors['firstname']", Matchers.is("Author firstname is required")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.errors['email']", Matchers.is("Invalid email")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.status", Matchers.is(HttpStatus.BAD_REQUEST.name())));
+        verify(this.authorService, never()).create(any(AuthorRequest.class));
     }
 }
