fe010347bfcbe9a6b29e517bcb222c74424d76ef
leonel ka <waboleonel@gmail.com>
2025-05-17 03:42:59 +0100

    Update web controller

--- src/main/java/com/workbeattalent/books/web/WebController.java
+++ src/main/java/com/workbeattalent/books/web/WebController.java
@@ -1,4 +1,29 @@
 package com.workbeattalent.books.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class WebController {
+
+    @GetMapping
+    public String home() {
+        return "pages/home";
+    }
+
+    @GetMapping(path = {"/books"})
+    public String getBooks() {
+        return "pages/books";
+    }
+
+    @GetMapping(path = {"/contact"})
+    public String getIntouch() {
+        return "pages/contact";
+    }
+
+    @GetMapping(path = {"/swagger"})
+    public String docs() {
+        return "redirect:/swagger-ui.html";
+    }
+
 }
